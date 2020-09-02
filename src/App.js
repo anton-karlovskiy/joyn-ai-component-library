7197daca84981c34fdbaf77b23f127b305c6bfe5
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-02 17:03:13 +0300

    [infra] set up MUI and customized MUI theme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
+
 import React from 'react';
-import logo from './logo.svg';
+// ray test touch <
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import theme from 'styles/theme';
+import Layout from 'utils/hocs/Layout';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout />
+    </ThemeProvider>
   );
-}
+};
+// ray test touch >
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,106 @@
+
+// ray test touch <
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#33d8c7',
+      main: '#00cfba',
+      dark: '#009082',
+      contrastText: '#fff'
+    },
+    secondary: {
+      light: '#7088b7',
+      main: '#4d6ba5',
+      dark: '#354a73',
+      contrastText: '#fff'
+    },
+    error: {
+      light: '#ff3333',
+      main: '#ff0000',
+      dark: '#b20000',
+      contrastText: '#fff'
+    },
+    text: {
+      primary: '#2f2f2f',
+      secondary: '#fff'
+    },
+    background: {
+      default: '#f5f6f8'
+    }
+  },
+  overrides: {
+    MuiButton: {
+      // TODO: small, large
+      // https://material-ui.com/customization/globals/
+      // https://material-ui.com/api/button/
+      root: {
+        minWidth: 96,
+        minHeight: 40
+      }
+    }
+  },
+  typography: {
+    htmlFontSize: 10,
+    button: {
+      fontSize: '1.2rem',
+      letterSpacing: '-0.33px'
+    },
+    h3: {
+      fontSize: '2.4rem',
+      letterSpacing: '-0.67px'
+    },
+    h4: {
+      fontSize: '1.8rem',
+      letterSpacing: '-0.5px'
+    },
+    h5: {
+      fontSize: '1.4rem',
+      letterSpacing: '-0.39px',
+    },
+    h6: {
+      fontSize: '1.2rem',
+      letterSpacing: '-0.33px'
+    },
+    subtitle1: {
+      fontSize: '1rem',
+      letterSpacing: '-0.28px',
+      fontWeight: 700
+    },
+    body1: {
+      fontSize: '1rem',
+      letterSpacing: '-0.28px'
+    },
+    subtitle2: {
+      fontSize: '.8rem',
+      letterSpacing: '-0.22px',
+      fontWeight: 700
+    },
+    body2: {
+      fontSize: '.8rem',
+      letterSpacing: '-0.22px'
+    }
+  },
+  custom: {
+    palette: {
+      blueGrey: '#424f62',
+      // network color schema
+      grey: '#4A4A4A', // equipment
+      red: '#F66', // gas
+      green: '#6C6', // oil
+      blue: '#387DF4', // water
+      darkBlue: '#34528b'
+    },
+    layout: {
+      openedDrawerWidth: 272,
+      closedDrawerWidth: 64,
+      upperSubToolbarHeight: 80,
+      lowerSubToolbarHeight: 64,
+      networkPopperWidth: 280
+    }
+  }
+});
+
+export default theme;
+// ray test touch >
