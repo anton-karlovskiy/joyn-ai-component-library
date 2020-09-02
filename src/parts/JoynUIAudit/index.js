59820e3cb92b10e527124a6c24611400f4d339b7
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-03 00:49:03 +0300

    [feat] implemented the contained button with an icon

--- src/parts/JoynUIAudit/index.js
+++ src/parts/JoynUIAudit/index.js
@@ -3,10 +3,14 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import ContainedButton from 'components/UI/ContainedButton';
+import { ReactComponent as AddIcon } from 'assets/icons/svgs/add.svg';
 
 const useStyles = makeStyles(theme => ({
   joynUiAudit: {
-    margin: theme.spacing(1)
+    margin: theme.spacing(1),
+    '& > *': {
+      margin: theme.spacing(.5)
+    }
   }
 }));
 
@@ -16,6 +20,11 @@ const JoynUIAudit = () => {
   return (
     <div className={classes.joynUiAudit}>
       <ContainedButton color='primary'>
+        New
+      </ContainedButton>
+      <ContainedButton
+        color='primary'
+        startIcon={<AddIcon fill='currentColor' />}>
         New
       </ContainedButton>
     </div>
