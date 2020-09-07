c21eeb8e31bded98f48dd709dfa133e25be4e2ed
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-07 12:51:55 +0300

    [chore] updated the storybook view layout for the button set

--- src/stories/JoynButton.stories.js
+++ src/stories/JoynButton.stories.js
@@ -1,25 +1,55 @@
 
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 import JoynButton from 'components/JoynButton';
 import BUTTON_TYPES from 'utils/constants/button-types';
+import { ReactComponent as SaveIcon } from 'assets/icons/svgs/save.svg';
 
-const Template = args => <JoynButton {...args} />;
+const useStyles = makeStyles(theme => ({
+  root: {
+    margin: theme.spacing(1),
+    '& > *': {
+      margin: theme.spacing(.5)
+    }
+  }
+}));
 
-const PrimaryNormal = Template.bind({});
-PrimaryNormal.args = {
+const Template = args => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <JoynButton {...args} />
+      <JoynButton
+        {...args}
+        startIcon={<SaveIcon />} />
+      <JoynButton
+        {...args}
+        disabled
+        startIcon={<SaveIcon />} />
+      <JoynButton
+        {...args}
+        loading
+        startIcon={<SaveIcon />} />
+    </div>
+  );
+};
+
+const Primary = Template.bind({});
+Primary.args = {
   type: BUTTON_TYPES.PRIMARY,
   children: 'Primary',
 };
 
-const SecondaryNormal = Template.bind({});
-SecondaryNormal.args = {
+const Secondary = Template.bind({});
+Secondary.args = {
   type: BUTTON_TYPES.SECONDARY,
   children: 'Secondary',
 };
 
-const TertiaryNormal = Template.bind({});
-TertiaryNormal.args = {
+const Tertiary = Template.bind({});
+Tertiary.args = {
   type: BUTTON_TYPES.TERTIARY,
   children: 'Tertiary',
 };
@@ -30,7 +60,7 @@ export default {
 };
 
 export {
-  PrimaryNormal,
-  SecondaryNormal,
-  TertiaryNormal
+  Primary,
+  Secondary,
+  Tertiary
 };
