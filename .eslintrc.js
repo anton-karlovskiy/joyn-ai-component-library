0e9a2532ec25d39f9cd8aaf8ce4dfac0c1dab0ed
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2020-09-19 21:23:35 +0300

    [fix] polished eslinting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,15 @@ module.exports = {
     node: true
   },
   extends: [
+    'eslint:recommended',
     'react-app',
     'google',
-    // TODO: it looks like prettier is not working
-    'plugin:prettier/recommended'
+    /**
+     * TODO:
+     * It looks like prettier is not working properly.
+     * It overrides some rules from above extends.
+     */
+    // 'plugin:prettier/recommended'
   ],
   parser: 'babel-eslint',
   parserOptions: {
@@ -26,34 +31,16 @@ module.exports = {
     },
   },
   plugins: [
-    'react'
+    'react',
+    'import'
   ],
   rules: {
-    'comma-dangle': ['warn', 'never'],
+    // ESLinting rules
+    'comma-dangle': ['error', 'never'],
     'no-console': 'warn',
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'comma-spacing': ['error', { 'before': false, 'after': true }],
     'indent': ['error', 2],
-    'object-curly-spacing': ['warn', 'always'],
     'quote-props': ['error', 'as-needed'],
-    'react/prop-types': 'warn',
-    'no-trailing-spaces': ['error', { 'skipBlankLines': true, 'ignoreComments': true }],
-    'react/jsx-first-prop-new-line': ['error', 'multiline'],
-    'react/jsx-max-props-per-line': ['error', { 'maximum': 1, 'when': 'always' }],
-    'react/jsx-closing-tag-location': 'error',
-    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
-    'react/jsx-curly-spacing': ['error', { 'when': 'never', 'children': true }],
-    'react/jsx-tag-spacing': ['error', {
-      'closingSlash': 'never',
-      'beforeSelfClosing': 'always',
-      'afterOpening': 'never',
-      'beforeClosing': 'never'
-    }],
-    'jsx-quotes': ['error', 'prefer-single'],
-    // TODO: it looks like prettier is not working
-    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-
+    'capitalized-comments': 'off',
     'max-len': ['warn',
       {
         code: 130
@@ -72,6 +59,49 @@ module.exports = {
       {
         exceptions: ['/']
       }
-    ]
+    ],
+    // MEMO: They are set by extends
+    // 'quotes': ['error', 'single'],
+    // 'semi': ['error', 'always'],
+    // 'comma-spacing': ['error', { 'before': false, 'after': true }],
+    'object-curly-spacing': ['error', 'always'],
+    // 'no-trailing-spaces': ['error', { 'skipBlankLines': false, 'ignoreComments': false }],
+    // 'no-multi-spaces': 'error',
+    // 'space-after-keywords': ['error', 'always'],
+    // 'keyword-spacing': ['error', { before: true, after: true }],
+    // 'block-spacing': ['error', 'never'],
+    // 'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    // 'space-in-parens': ['error', 'never'],
+    // 'capitalized-comments': ['warn', 'always', {
+    //   'ignorePattern': 'pragma|ignored',
+    //   'ignoreInlineComments': true
+    // }],
+    
+    // React linting rules
+    'react/prop-types': 'off',
+    'react/jsx-first-prop-new-line': ['error', 'multiline'],
+    'react/jsx-max-props-per-line': ['error', { 'maximum': 1, 'when': 'always' }],
+    'react/jsx-closing-tag-location': 'error',
+    'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+    'react/jsx-curly-spacing': ['error', { 'when': 'never', 'children': true }],
+    'react/jsx-tag-spacing': ['error', {
+      'closingSlash': 'never',
+      'beforeSelfClosing': 'always',
+      'afterOpening': 'never',
+      'beforeClosing': 'never'
+    }],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'react/jsx-closing-bracket-location': ['error', { selfClosing: 'after-props', nonEmpty: 'after-props' }],
+    // TODO: It looks like it's not working as expected
+    'react/jsx-props-no-multi-spaces': 'error',
+
+    // Prettier linting rules
+    /**
+     * TODO:
+     * It looks like prettier is not working properly.
+     * It overrides some rules from above extends.
+     */
+    // 'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'import/exports-last': 'error'
   }
 };
